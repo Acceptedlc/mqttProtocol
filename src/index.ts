cb39cdb7980581713c3d189cdb26d333dd0c798a
lichao <lichao@gagougroup.com>
2018-07-04 09:21:54 +0800

    feat: add index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+
+import {MqttClientSocket, MqttClientSocketOptions} from "./socket/mqtt-client-socket";
+import {MqttServerSocket} from "./socket/mqtt-server-socket";
+
+export {MqttClientSocket, MqttServerSocket, MqttClientSocketOptions}
--- test.js
+++
@@ -1,5 +0,0 @@
-const net = require("net");
-const mqttCon = require('mqtt-connection');
-
-var stream = net.createConnection(5555, "localhost");
-var aaaa = mqttCon(stream);
